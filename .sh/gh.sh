1c4ff11181649a356d5dce13db098e02ff12d7e1
Thomasvader <lwmwll@163.com>
2020-09-28 14:26:57 +0800

    修改gh命令行，直接提价gh-pages

--- .sh/gh.sh
+++ .sh/gh.sh
@@ -42,19 +42,22 @@ echo - "$before'▶ dist文件夹移动成功.'$rear"
 sleep 1s
 
 # 切换分支
+echo -e "$before'▶ 切换分支.'$rear"
 git checkout gh-pages
+echo -e "$before'▶ 分支切换成功.'$rear"
+
 # 新建分支(从gh-pages中新建一个分支)
-echo -e "$before'▶ 创建新分支.'$rear"
-git branch "$gh_branch"
-echo -e "$before'▶ 分支创建成功.'$rear"
+# echo -e "$before'▶ 创建新分支.'$rear"
+# git branch "$gh_branch"
+# echo -e "$before'▶ 分支创建成功.'$rear"
 
 # 切换分支
-echo -e "$before'▶ 切换分支.'$rear"
-git checkout "$gh_branch"
-echo -e "$before'▶ 成功切换到 "$gh_branch" 分支.'$rear"
+# echo -e "$before'▶ 切换分支.'$rear"
+# git checkout "$gh_branch"
+# echo -e "$before'▶ 成功切换到 "$gh_branch" 分支.'$rear"
 
 # 复制文件
-echo -e "$before'▶ 开始将所有文件移到dist下.'$rear"
+echo -e "$before'▶ 开始将所有文件移到全局静态文件下.'$rear"
 cd ..
 cp -rvf Aftersoil-wiki/dist/* Aftersoil-wiki
 cd Aftersoil-wiki/
@@ -64,6 +67,7 @@ sleep 1s
 
 echo  -e "$before'▶ 查看当前分支状态 '$rear"
 git status
+echo -e "$before'▶ 移动成功暂未提交 '$rear"
 
 # 开始远程提交
 echo -e "$before'▶ 开始提交暂存区,请稍后... '$rear"
@@ -72,28 +76,28 @@ echo -e "$before'▶ 文件已全部提交到暂存区. '$rear"
 
 git status
 
-echo -e "$before'请输入您要提交的commit: '$rear"
+echo -e "$before'▶请输入您要提交的commit: '$rear"
 read commits
 git commit -m "$commits"
+echo -e "$before'▶ 输入成功，commit 提交出成功'$rear"
 
+echo -e "$before'▶ 开始向远 gh-pages 提交合并.'$rear"
 git push origin "$gh_branch"
-echo -e "$before'▶ 静态文件已成功提交，请转到GitHub合并分支.'$rear"
+echo -e "$before'▶ 静态文件已成功提交，请转到GitHub查看是否提交成功.'$rear"
 
 sleep 1s
 
-echo -e "$before'▶ 切换分支并删除 "$gh_branch" 和 gh-pages 分支。'$rear"
+echo -e "$before'▶ 查看您本地分支'$rear"
 git branch
+echo -e "$before'▶ 您本地分支如下'$rear"
 echo -e "$before'▶ 请输入您的开发分支名称。'$rear"
-
 read dev
-
-echo  -e "$before'▶ 正在删除分支请稍后... '$rear"
-
+echo  -e "$before'▶ 正在切换分支请稍后... '$rear"
 git checkout "$dev"
-git branch -D "$gh_branch" gh-pages
+git branch -D gh-pages
 rm -rf dist
 git branch
-echo -e "$before'▶ 分支删除成功代码成功执行，欢迎下次提交.'$rear"
+echo -e "$before'▶ 切换成功，欢迎下次提交.'$rear"
 
 # 退出脚本
 exit
