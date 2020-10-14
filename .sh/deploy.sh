f7d1a28e0794ebefb7a47ad2434b8bffaf7d77c1
Thomasvader <lwmwll@163.com>
2020-10-14 09:25:16 +0800

    update deploy.sh pull

--- .sh/deploy.sh
+++ .sh/deploy.sh
@@ -9,18 +9,22 @@ sleep 1s
 RemotePull () {
   sleep 1s
 
-  # Please wait a moment to start pulling new requests from GitHub.
-  echo -e "$before 开始从 GitHub 拉取新的请求请稍后... $rear"
+  echo -e "$before▶ 正在获取远分支请稍后... $rear"
+  git branch -r
+  echo -e "$before▶ 远程分支获取完成. $rear"
 
-  echo -e "$before 请输入你要拉取的分支 $rear"
+  # Please wait a moment to start pulling new requests from GitHub.
+  echo -e "$before▶ 开始从 GitHub 拉取新的请求请稍后... $rear"
+
+  echo -e "$before▶ 请输入你要拉取的分支 $rear"
   read temp_branch
 
   # https://<USERNAME>.github.io/<REPO>  git@github.com:Aftersoil/Aftersoil-wiki.git
   git pull origin "$temp_branch"
-  echo -e "$before 分支拉取成功 $rear"
+  echo -e "$before▶ 分支拉取成功 $rear"
 
   # The pull is successful and merged into the current branch.
-  echo -e "$before 拉取成功并合并到当前分支. $rear"
+  echo -e "$before▶ 拉取成功并合并到当前分支. $rear"
 
   sleep 1s
 }
@@ -29,13 +33,13 @@ Bale() {
   sleep 1s
 
   # Please wait to start packaging the project...
-  echo -e "$before 开始打包项目请稍后... $rear"
+  echo -e "$before▶ 开始打包项目请稍后... $rear"
 
   # Generate static files.
   npm run build
 
   # Project package completed.
-  echo -e "$before 项目打包完成. $rear"
+  echo -e "$before▶ 项目打包完成. $rear"
 
   sleep 1s
 }
@@ -59,7 +63,7 @@ case $var in
     exit
   ;;
   *)
-    echo -e "$before 请输入正确的格式. $rear"
+    echo -e "$before▶ 请输入正确的格式. $rear"
     npm run deploy
   ;;
 esac
